dc89e9cffcf9f1384ac23c5ea145509b4ac6e902
iDiffusion <ikaikalee22@gmail.com>
2020-02-10 12:47:19 -0500

    Add i2c setup

--- src/arduino/main.ino
+++ src/arduino/main.ino
@@ -1,6 +1,8 @@
 /*
-  This program will obtain all the sensor data and send it to the raspberry pi
+  Obtain all the sensor data and send it to the raspberry pi
 */
+
+#include <Wire.h>
 
 void setup() {
 
@@ -8,4 +10,11 @@ void setup() {
 
 void loop() {
 
+}
+
+void receiveEvent(int howMany) {
+  while (Wire.available()) { // loop through all but the last
+    char c = Wire.read(); // receive byte as a character
+    //TODO interpret data recieved on the I2C Bus
+  }
 }
--- src/pi/main.py
+++ src/pi/main.py
@@ -1,38 +1,9 @@
 import os
 import time
-from Sensor import Humidity
+from smbus import SMBus
 
 debug = True
-hum = Humidity(4);
+bus = SMBus(1) # indicates /dev/ic2-1
+# To check for enabled type "ls /dev/*i2c*" in cmd
 
-try:
-    f = open('/media/pi/data/humidity.csv', 'a+')
-    if os.stat('/media/pi/data/humidity.csv').st_size == 0:
-        f.write('Date,Time,Temperature,Humidity\r\n')
-
-    while True:
-        # Print date
-        if debug:
-            print('{0}\t'.format(time.strftime('%H:%M')))
-
-        # Humidity sensor data
-        humidity, temperature = hum.getValue()
-        if humidity is not None and temperature is not None:
-            f.write('{0},{1},{2:0.1f}*F,{3:0.1f}%\r\n'.format(time.strftime('%m/%d/%y'), time.strftime('%H:%M'), temperature, humidity))
-            if debug:
-                print('{0:0.1f}*F\t{1:0.1f}%'.format(temperature, humidity))
-        elif debug:
-            print("Failed to retrieve data from humidity sensor")
-
-        # Print new line
-        if debug:
-            print ('\n')
-
-        # Sleep for 30 seconds
-        time.sleep(30)
-
-except:
-    pass
-
-finally:
-    pass
+#TODO send commands to arduino to retrieve information
